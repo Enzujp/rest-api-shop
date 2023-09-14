560689671312470d28790d7fc85b5cac1a0087d0
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-09-14 16:42:16 +0100

    add create, patch, delete and get functionalities

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,49 +1,107 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const Product = require("../../src/models/product");
 
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: "Sending GET requests to /products"
+    Product.find() // to display all products
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        res.status(200);
+        res.json({
+            docs
+        })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500);
+        res.json({
+            error: err
+        })
     })
 })
 
 router.post('/', (req, res, next) => {
     // create a product
-    const product = {
-        name : req.body.name, // should be passed in the frontend, would be stated in documentation
-        price : req.body.price
-    }
-    
-
-    res.status(201).json({
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(), // create new unique id
+        name: req.body.name,
+        price: req.body.price
+    });
+    product.save().then(result => {
+        console.log(result);
+        res.status(201).json({
         message : "Sending POST requests to /products",
         createdProduct : product
     })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500)
+        res.json({
+            error: err
+        })
+    });
+    
 })
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    if (id === 'special') {
-        res.status(200).json({message: "Great job discovering the special deal", id : id});
-    }
-    else {
-        res.status(200).json({
-            message: "This is the product you are looking for my friend."
+    Product.findById(id).exec()
+    .then( doc=> {
+        console.log("fetched from database", doc);
+        if (doc){
+            res.status(200).json(doc); // return status code and document json property
+        } else {
+            res.status(404).json({
+                message: "Cannot find details associated with this product ID"
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500);
+        res.json({
+            error: err
         })
-    }
+    })
 })
 
 router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: "This product has been updated"
+    const id = req.params.productId;
+    const updatePars = {}; // set empty object to hold updates to be made
+    for (const pars of req.body) { // parameters are json objects, tap into key and value ppties
+        updatePars[pars.itemName] = pars.value
+    }
+    Product.updateOne({_id: id }, {$set: updatePars}).exec()
+    .then(result => {
+        console.log(result);
+        res.status(200);
+        res.json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500);
+        res.json({errorMessage: err})
     })
 })
 
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: "This product has been deleted"
-    })
+    const id = req.params.productId;
+    Product.remove({ 
+        _id: id // remove _id property from database with value of id
+     }).exec()
+     .then(result => {
+        res.status(200).json(result)
+     })
+     .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error : err
+        })
+     });
 })
 
 
